ca1c8ee8d83e489c7f7556082e112454f13000fd
Matthias Pigulla <mp@webfactory.de>
2025-05-02 15:48:50 +0200

    Replace the `hiera()` function with `lookup()`

--- manifests/init.pp
+++ manifests/init.pp
@@ -165,7 +165,7 @@ class supervisord(
 
   if $env_var {
     validate_hash($env_var)
-    $env_hash = hiera($env_var)
+    $env_hash = lookup($env_var)
     $env_string = hash2csv($env_hash)
   }
   elsif $_global_environment {
